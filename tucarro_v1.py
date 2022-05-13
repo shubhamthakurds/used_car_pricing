e51ed2ce28555ce7a930372e64a9ba47c30a3986
shubhamthakurds <105434561+shubhamthakurds@users.noreply.github.com>
2022-05-13 16:52:51 +0530

    Add files via upload

---
+++ tucarro_v1.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+import time
+import random
+import pandas as pd
+
+
+def get_all_elements():
+    return driver.find_elements_by_xpath(
+        "//*[@class='ui-search-result__content ui-search-link']"
+    )
+
+
+def get_url(var):
+    return var.get_attribute("href")
+
+
+def next_page():
+    return driver.find_elements_by_xpath(
+        "//*[@class='andes-pagination__button andes-pagination__button--next']"
+    )[-1].click()
+
+
+chromeOptions = webdriver.ChromeOptions()
+chromeOptions.add_argument(
+    r"--user-data-dir=C:\Users\shubham.thakur\AppData\Local\Google\Chrome\User Data"
+)
+chromeOptions.add_argument("--profile-directory=Profile 3")
+chromeOptions.add_argument("--disable-extensions")
+
+driver = webdriver.Chrome(
+    executable_path=r"C:\Users\shubham.thakur\Documents\chromedriver.exe",
+    options=chromeOptions,
+)
+driver.get("https://carros.tucarro.com.co/_NoIndex_True")
+driver.find_element_by_id("newCookieDisclaimerButton").click()
+
+max_page = int(
+    driver.find_element_by_class_name("andes-pagination__page-count").text.split()[-1]
+)
+
+url_list = []
+
+for i in range(0, max_page):
+    print(i)
+    time.sleep(random.randint(1, 5))
+    elements = get_all_elements()
+    for elem in elements:
+        url_list.append(get_url(elem))
+    time.sleep(random.randint(1, 10))
+    try:
+        next_page()
+    except:
+        break
+
+df = pd.DataFrame()
+df["URLS"] = url_list
+df.to_excel("C://Users/shubham.thakur/Downloads/URLS.xlsx")
